0676e05bb78845045d31ab1ea295f9a68e02184a
Aller Augusto de Souza <allerasouza@gmail.com>
2023-04-02 13:42:06 -0300

    feat: add global stylization

--- gitcollection/src/App.tsx
+++ gitcollection/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './appRoutes';
+import { GlobalStyle } from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 };
 
