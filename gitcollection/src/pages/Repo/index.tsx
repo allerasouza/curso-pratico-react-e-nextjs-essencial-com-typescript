96d5036d91ac06e10af88dd41bb20e0e00fd6336
Aller Augusto de Souza <allerasouza@gmail.com>
2023-04-05 21:57:37 -0300

    feat: add stylization for details page

--- gitcollection/src/pages/Repo/index.tsx
+++ gitcollection/src/pages/Repo/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Header, RepoInfo } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import { FiChevronLeft } from 'react-icons/fi';
 
 interface RepositoryParams {
   [repository: string]: string;
@@ -7,5 +11,39 @@ interface RepositoryParams {
 
 export const Repo: React.FC = () => {
   const { repository } = useParams<RepositoryParams>();
-  return <h1>Repo: {repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="GitCollection" />
+        <Link to="/">
+          <FiChevronLeft />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepoInfo>
+        <header>
+          <img src="" alt="Aller" />
+          <div>
+            <strong>aller/hudeing</strong>
+            <p>Repositorio teste</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>2330</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>210</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>79</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepoInfo>
+    </>
+  );
 };
