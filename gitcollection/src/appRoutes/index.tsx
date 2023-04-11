26db44ef4cac3d22e6800d802a7928ba3c66a107
Aller Augusto de Souza <allerasouza@gmail.com>
2023-04-11 20:50:05 -0300

    feat: add lazy loading to react components

--- gitcollection/src/appRoutes/index.tsx
+++ gitcollection/src/appRoutes/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Dashboard } from '../pages/Dashboard';
-import { Repo } from '../pages/Repo';
+// import { Dashboard } from '../pages/Dashboard';
+// import { Repo } from '../pages/Repo';
+const Dashboard = lazy(() => import('../pages/Dashboard'));
+const Repo = lazy(() => import('../pages/Repo'));
 
 export const AppRoutes: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/repositories/:repository" element={<Repo />} />
-    </Routes>
+    <Suspense fallback={'Loading...'}>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/repositories/:repository" element={<Repo />} />
+      </Routes>
+    </Suspense>
   );
 };
--- gitcollection/src/pages/Dashboard/index.tsx
+++ gitcollection/src/pages/Dashboard/index.tsx
@@ -20,7 +20,7 @@ interface GithubRepository {
   };
 }
 
-export const Dashboard: React.FC = () => {
+const Dashboard: React.FC = () => {
   const [repos, setRepos] = useState<GithubRepository[]>(() => {
     const storageRepos = localStorage.getItem('@GitColletion:repositories');
     if (storageRepos) {
@@ -100,3 +100,5 @@ export const Dashboard: React.FC = () => {
     </>
   );
 };
+
+export default Dashboard;
