7ffffa284ac9bd037d662daf62344d7072f213ce
Aller Augusto de Souza <allerasouza@gmail.com>
2023-04-11 21:09:07 -0300

    feat: add webpack magic comments to chunk and prefetch chunks

--- gitcollection/src/appRoutes/index.tsx
+++ gitcollection/src/appRoutes/index.tsx
@@ -2,8 +2,22 @@ import React, { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 // import { Dashboard } from '../pages/Dashboard';
 // import { Repo } from '../pages/Repo';
-const Dashboard = lazy(() => import('../pages/Dashboard'));
-const Repo = lazy(() => import('../pages/Repo'));
+const Dashboard = lazy(
+  () =>
+    import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: "dashboard" */
+      '../pages/Dashboard'
+    ),
+);
+const Repo = lazy(
+  () =>
+    import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: "repo" */
+      '../pages/Repo'
+    ),
+);
 
 export const AppRoutes: React.FC = () => {
   return (
