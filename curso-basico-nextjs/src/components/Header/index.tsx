b2ff8daa3cbf37f174c7b750b929caab0ec01387
Aller Augusto de Souza <allerasouza@gmail.com>
2023-04-17 09:08:02 -0300

    feat: add stylization to active navbar item

--- curso-basico-nextjs/src/components/Header/index.tsx
+++ curso-basico-nextjs/src/components/Header/index.tsx
@@ -1,13 +1,16 @@
+import Link from 'next/link';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
 
 export function Header() {
+  const { asPath } = useRouter();
   return (
     <header className={styles.container}>
-      <div  className={styles.content}>
+      <div className={styles.content}>
         <img src="/logo.svg" alt="DevNews!" />
         <nav>
-          <a href="#">Home</a>
-          <a href="#">Posts</a>
+          <Link href="/" className={asPath === '/' ? styles.active : ''}>Home</Link>
+          <Link href="/posts" className={asPath === '/posts' ? styles.active : ''}>Posts</Link>
         </nav>
       </div>
     </header>
