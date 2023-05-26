0b5ff5be8684768e0c47a8bf0dd6db2abc83740d
Aller Augusto de Souza <allerasouza@gmail.com>
2023-05-25 22:06:02 -0300

    feat: add lazy load component

--- curso-basico-nextjs/src/pages/calculo.tsx
+++ curso-basico-nextjs/src/pages/calculo.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+// import { Modal } from '../components/Modal';
+
+const Modal = dynamic(
+  () => import('../components/Modal').then(mod => mod.Modal),
+  { loading: () => <p>Loading ...</p>, ssr: false },
+);
+
 export default function Calculo() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   async function handleSum() {
     const calc = (await import('../libs/calc')).default;
     alert(calc.sum(5, 6));
+  }
+
+  function handleModalVisible() {
+    setModalVisible(!modalVisible);
   }
 
   return (
     <div>
       <h1>Calculo</h1>
       <button onClick={handleSum}>Somar</button>
+      <button onClick={handleModalVisible}>Modal</button>
+      {modalVisible && <Modal />}
     </div>
   );
 }
