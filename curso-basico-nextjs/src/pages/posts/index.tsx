e514d63187adb621be3d79e2ffc782dcfbc81a38
Aller Augusto de Souza <allerasouza@gmail.com>
2023-05-31 08:38:22 -0300

    feat: apply post page stylization

--- curso-basico-nextjs/src/pages/posts/index.tsx
+++ curso-basico-nextjs/src/pages/posts/index.tsx
@@ -1,6 +1,7 @@
 import SEO from '@/src/components/SEO';
 import Link from 'next/link';
 import { GetStaticProps } from 'next';
+import styles from './posts.module.scss';
 
 export interface Post {
   id: string;
@@ -15,14 +16,12 @@ export default function Posts() {
   return (
     <>
       <SEO title="Posts" />
-      <main>
-        <div>
+      <main className={styles.container}>
+        <div className={styles.posts}>
           <Link href="#">
-            <a>
-              <time>30 de maio de 2023</time>
-              <strong>Titulo</strong>
-              <p>Paragrafo</p>
-            </a>
+            <time>30 de maio de 2023</time>
+            <strong>Titulo</strong>
+            <p>Paragrafo</p>
           </Link>
         </div>
       </main>
