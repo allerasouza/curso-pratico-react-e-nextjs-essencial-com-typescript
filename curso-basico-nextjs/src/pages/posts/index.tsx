dab776976bd81779ce0a11faa1428db6048b6754
Aller Augusto de Souza <allerasouza@gmail.com>
2023-06-14 07:24:42 -0300

    feat: fetch data from prismic

--- curso-basico-nextjs/src/pages/posts/index.tsx
+++ curso-basico-nextjs/src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import SEO from '@/src/components/SEO';
 import Link from 'next/link';
 import { GetStaticProps } from 'next';
 import styles from './posts.module.scss';
+import { getPrismicClient } from '../../services/prismic';
 
 export interface Post {
   id: string;
@@ -30,6 +31,12 @@ export default function Posts() {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const documentType = 'post';
+  const response = await prismic.getAllByType(documentType, {
+    fetch: ['post.title', 'post.content'],
+  });
+  console.log('response:', response);
   return {
     props: {},
     revalidate: 60 * 60 * 12, // 12 horas
