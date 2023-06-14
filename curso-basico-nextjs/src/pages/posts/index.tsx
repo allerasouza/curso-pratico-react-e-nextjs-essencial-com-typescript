cbfe2f51b3cac957df4fc6d21cd756ca7f94961a
Aller Augusto de Souza <allerasouza@gmail.com>
2023-06-14 20:29:53 -0300

    feat: show posts on frontend

--- curso-basico-nextjs/src/pages/posts/index.tsx
+++ curso-basico-nextjs/src/pages/posts/index.tsx
@@ -3,27 +3,33 @@ import Link from 'next/link';
 import { GetStaticProps } from 'next';
 import styles from './posts.module.scss';
 import { getPrismicClient } from '../../services/prismic';
+import * as prismicH from '@prismicio/helpers';
+import { RTParagraphNode } from '@prismicio/types';
 
 export interface Post {
-  id: string;
+  slug: string;
   title: string;
+  excerpt: string;
+  updateAt: string;
 }
 
 interface PostsProps {
   posts: Post[];
 }
 
-export default function Posts() {
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <SEO title="Posts" />
       <main className={styles.container}>
         <div className={styles.posts}>
-          <Link href="#">
-            <time>30 de maio de 2023</time>
-            <strong>Titulo</strong>
-            <p>Paragrafo</p>
-          </Link>
+          {posts.map(post => (
+            <Link href="#" key={post.slug}>
+              <time>{post.updateAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </Link>
+          ))}
         </div>
       </main>
     </>
@@ -36,9 +42,28 @@ export const getStaticProps: GetStaticProps = async () => {
   const response = await prismic.getAllByType(documentType, {
     fetch: ['post.title', 'post.content'],
   });
-  console.log('response:', response);
+  const posts = response.map(post => {
+    return {
+      slug: post.uid,
+      title: prismicH.asText(post.data.title),
+      excerpt:
+        (
+          post.data.content.find(content => content.type === 'paragraph') as
+            | RTParagraphNode
+            | undefined
+        )?.text ?? '',
+      updateAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        },
+      ),
+    };
+  });
   return {
-    props: {},
+    props: { posts },
     revalidate: 60 * 60 * 12, // 12 horas
   };
 };
