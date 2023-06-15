52859d45d1c5971c8e536d78daab495271eed992
Aller Augusto de Souza <allerasouza@gmail.com>
2023-06-15 08:07:46 -0300

    test: ensure ActiveLink has active class if the link is currently active

--- curso-basico-nextjs/src/components/ActiveLink/ActiveLink.spec.tsx
+++ curso-basico-nextjs/src/components/ActiveLink/ActiveLink.spec.tsx
@@ -11,7 +11,16 @@ jest.mock('next/router', () => {
   };
 });
 
-it('should renders correctly', () => {
-  render(<ActiveLink href="/" activeClassName="active" content="Home" />);
-  expect(screen.getByText('Home')).toBeInTheDocument();
+describe('ActiveLink component', () => {
+  it('should renders correctly', () => {
+    render(<ActiveLink href="/" activeClassName="active" content="Home" />);
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+
+  it('should adds active class if the link is currently active', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active" content="Home" />,
+    );
+    expect(getByText('Home')).toHaveClass('active');
+  });
 });
